cd6b1e6476a7518ddb010a4290390b76b2ef67fc
Sreehari-S-Madhavan <ssreehari120@gmail.com>
2025-06-15 07:57:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const segments = [
 
 let truths = [];
 let dares = [];
+let mysteryTasks = [];
+let challengeBattles = [];
 
 // Load JSON files
 fetch('truth-questions.json')
@@ -13,6 +15,14 @@ fetch('truth-questions.json')
 fetch('dare-questions.json')
   .then(res => res.json())
   .then(data => dares = data.dares);
+
+fetch('mystery-tasks.json')
+  .then(res => res.json())
+  .then(data => mysteryTasks = data.mysteryTasks);
+
+fetch('challenge-battles.json')
+  .then(res => res.json())
+  .then(data => challengeBattles = data.challengeBattles);
 
 const canvas = document.getElementById("wheel");
 const ctx = canvas.getContext("2d");
@@ -48,8 +58,28 @@ spinBtn.addEventListener("click", () => {
   } else if (chosen === 'Dare') {
     const task = dares[Math.floor(Math.random() * dares.length)];
     output = `Dare: ${task}`;
+  } else if (chosen === 'Mystery Task') {
+    const task = mysteryTasks[Math.floor(Math.random() * mysteryTasks.length)];
+    output = `Mystery Task: ${task}`;
+  } else if (chosen === 'Challenge Battle') {
+    const challenge = challengeBattles[Math.floor(Math.random() * challengeBattles.length)];
+    output = `Challenge Battle: ${challenge}`;
+  } else if (chosen === 'All Answer') {
+    const question = truths[Math.floor(Math.random() * truths.length)];
+    output = `All Players Answer: ${question}`;
+  } else if (chosen === 'Double Dare') {
+    const dare1 = dares[Math.floor(Math.random() * dares.length)];
+    let dare2;
+    do {
+      dare2 = dares[Math.floor(Math.random() * dares.length)];
+    } while (dare1 === dare2);
+    output = `Double Dare: 1) ${dare1}  2) ${dare2}`;
+  } else if (chosen === 'Swap Turn') {
+    output = `Swap Turn: Choose a player to take your turn.`;
+  } else if (chosen === 'Skip Turn') {
+    output = `Skip Turn: You lose your turn.`;
   } else {
-    output = `${chosen} - special action!`;
+    output = `${chosen} - Special action!`;
   }
 
   result.innerText = output;
